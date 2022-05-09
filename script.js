9e0a93da3e5bbaadcaea583ecb3aa0c578403de4
Trithereon <eliottrassa@gmail.com>
2022-05-09 01:40:52 -0400

    Capitalize all inputs, to accomodate case-insensitive user input

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function game(){
     let computerScore = 0;
     for (let i=1; i<=5; i++){
         let playerSelection = prompt('Rock, Paper, or Scissors?'); //Must be made case-insensitive
+        playerSelection = playerSelection.toUpperCase();
         let computerSelection = computerPlay();
 
         function computerPlay(){
-            const inputs = ['Rock', 'Paper', 'Scissors']; 
+            const inputs = ['ROCK', 'PAPER', 'SCISSORS']; 
             return inputs[Math.floor(Math.random() * 3)]; 
             //Math.floor(Math.random() * 3); Randomly generates a value 0, 1, or 2
         }
@@ -15,29 +16,30 @@ function game(){
             //conditional statements to determine the winner. switch? if/else if?
             if (playerSelection === computerSelection){ 
                 return 'Draw! Nobody wins this round.';
-            }else if (playerSelection === 'Rock' && computerSelection === 'Paper'){ 
+            }else if (playerSelection === 'ROCK' && computerSelection === 'PAPER'){ 
                 computerScore++;
-                return 'You lose! Paper beats Rock';
-            }else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+                return 'You lose the round! Paper beats Rock';
+            }else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
                 playerScore++;
-                return 'You win! Rock beats Scissors';
-            }else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+                return 'You win the round! Rock beats Scissors';
+            }else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
                 playerScore++;
-                return 'You win! Paper beats Rock';
-            }else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+                return 'You win the round! Paper beats Rock';
+            }else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
                 computerScore++;
-                return 'You lose! Scissors beat Paper';
-            }else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+                return 'You lose the round! Scissors beat Paper';
+            }else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
                 computerScore++;
-                return 'You lose! Rock beats Scissors';
-            }else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+                return 'You lose the round! Rock beats Scissors';
+            }else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
                 playerScore++;
-                return 'You win! Scissors beat Paper';   
+                return 'You win the round! Scissors beat Paper';   
             }
         }
 
         //return string declaring the winner, with details
-        console.log(`You played ${playerSelection} and Computer played ${computerSelection}`);
+        //uncomment line below to display round details. Commented it to clean up the output.
+        //console.log(`You played ${playerSelection} and Computer played ${computerSelection}`);
         console.log(playRound(playerSelection, computerSelection));
     }
 
